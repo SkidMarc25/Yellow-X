a1b3f5f0074982acab9db9ef56b0e942a753c383
SkidMarc25 <marcstuts08@gmail.com>
2025-07-17 22:30:34 -0500

    Update RockTunnelB1F.asm

--- data/wild/maps/RockTunnelB1F.asm
+++ data/wild/maps/RockTunnelB1F.asm
@@ -1,14 +1,14 @@
 RockTunnelB1FWildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 20, ZUBAT
-	db 17, GEODUDE
+	db 17, ONIX
 	db 18, MACHOP
-	db 21, ZUBAT
+	db 21, GRAVELER
 	db 22, ZUBAT
 	db 21, GEODUDE
 	db 20, MACHOP
 	db 14, ONIX
-	db 18, ONIX
+	db 25, MACHOKE
 	db 22, ONIX
 	end_grass_wildmons
 
