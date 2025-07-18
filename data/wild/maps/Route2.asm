d147896a6c1698a68df6ddb51926aa681076644e
SkidMarc25 <marcstuts08@gmail.com>
2025-07-17 19:50:27 -0500

    Update Route2.asm

--- data/wild/maps/Route2.asm
+++ data/wild/maps/Route2.asm
@@ -3,12 +3,12 @@ Route2WildMons:
 	db  3, RATTATA
 	db  3, PIDGEY
 	db  4, RATTATA
-	db  4, NIDORAN_M
-	db  4, NIDORAN_F
+	db  4, CATERPIE
+	db  4, WEEDLE
 	db  5, PIDGEY
 	db  6, NIDORAN_M
 	db  6, NIDORAN_F
-	db  7, PIDGEY
+	db  7, JIGGLYPUFF
 	db  7, PIDGEY
 	end_grass_wildmons
 
