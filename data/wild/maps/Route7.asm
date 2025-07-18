91ee27731c6c558f5b4014076fd4ac5d6ba78bb3
SkidMarc25 <marcstuts08@gmail.com>
2025-07-17 22:10:29 -0500

    Update Route7.asm

--- data/wild/maps/Route7.asm
+++ data/wild/maps/Route7.asm
@@ -1,15 +1,15 @@
 Route7WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 20, PIDGEY
-	db 22, PIDGEY
-	db 20, RATTATA
+	db 22, RATTATA
+	db 20, MEOWTH
 	db 15, ABRA
-	db 19, ABRA
-	db 24, PIDGEOTTO
-	db 26, ABRA
 	db 19, JIGGLYPUFF
+	db 24, PIDGEOTTO
+	db 26, PIKACHU
+	db 19, VENONAT
 	db 24, JIGGLYPUFF
-	db 24, JIGGLYPUFF
+	db 24, BUTTERFREE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
