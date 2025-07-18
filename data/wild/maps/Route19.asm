1d7d767b30db59d1b7dd13cf5641304e03ec14dd
SkidMarc25 <marcstuts08@gmail.com>
2025-07-17 22:18:56 -0500

    Update Route19.asm

--- data/wild/maps/Route19.asm
+++ data/wild/maps/Route19.asm
@@ -5,12 +5,12 @@ Route19WildMons:
 	def_water_wildmons 5 ; encounter rate
 	db  5, TENTACOOL
 	db 10, TENTACOOL
+	db 15, SEEL
+	db 15, HORSEA
+	db 10, SEEL
 	db 15, TENTACOOL
-	db  5, TENTACOOL
-	db 10, TENTACOOL
-	db 15, TENTACOOL
-	db 20, TENTACOOL
+	db 20, STARYU
 	db 30, TENTACOOL
 	db 35, TENTACOOL
-	db 40, TENTACOOL
+	db 40, TENTACRUEL
 	end_water_wildmons
