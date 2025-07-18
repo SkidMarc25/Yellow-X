22d61ca33b89386ba7067fe879984de50ce4543f
SkidMarc25 <marcstuts08@gmail.com>
2025-07-17 22:02:41 -0500

    Update Route24.asm

--- data/wild/maps/Route24.asm
+++ data/wild/maps/Route24.asm
@@ -2,11 +2,11 @@ Route24WildMons:
 	def_grass_wildmons 25 ; encounter rate
 	db 12, ODDISH
 	db 12, BELLSPROUT
-	db 13, PIDGEY
+	db 13, PSYDUCK
 	db 14, ODDISH
 	db 14, BELLSPROUT
 	db 15, PIDGEY
-	db 13, VENONAT
+	db 13, PSYDUCK
 	db 16, VENONAT
 	db 17, PIDGEY
 	db 17, PIDGEOTTO
