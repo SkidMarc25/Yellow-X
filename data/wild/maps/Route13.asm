626c04095e77d8de33b1fd947da046e95ace8d9d
SkidMarc25 <marcstuts08@gmail.com>
2025-07-17 22:17:15 -0500

    Update Route13.asm

--- data/wild/maps/Route13.asm
+++ data/wild/maps/Route13.asm
@@ -1,11 +1,11 @@
 Route13WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 25, ODDISH
-	db 25, BELLSPROUT
+	db 25, POLIWAG
+	db 25, KRABBY
 	db 28, PIDGEOTTO
-	db 28, PIDGEY
-	db 27, ODDISH
-	db 27, BELLSPROUT
+	db 28, KRABBY
+	db 27, PSYDUCK
+	db 27, POLIWHIRL
 	db 29, GLOOM
 	db 29, WEEPINBELL
 	db 26, FARFETCHD
@@ -14,13 +14,13 @@ Route13WildMons:
 
 	def_water_wildmons 3 ; encounter rate
 	db 15, SLOWPOKE
+	db 15, SEEL
+	db 15, HORSEA
 	db 15, SLOWPOKE
+	db 15, SEEL
+	db 15, HORSEA
 	db 15, SLOWPOKE
-	db 15, SLOWPOKE
-	db 15, SLOWPOKE
-	db 15, SLOWPOKE
-	db 15, SLOWPOKE
-	db 15, SLOWPOKE
+	db 15, DRATINI
 	db 15, SLOWBRO
 	db 20, SLOWBRO
 	end_water_wildmons
