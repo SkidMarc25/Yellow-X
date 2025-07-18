adb9fc816e2c63a32cf5ee52413fbe7562386c89
SkidMarc25 <marcstuts08@gmail.com>
2025-07-17 22:09:06 -0500

    Update Route8.asm

--- data/wild/maps/Route8.asm
+++ data/wild/maps/Route8.asm
@@ -3,11 +3,11 @@ Route8WildMons:
 	db 20, PIDGEY
 	db 22, PIDGEY
 	db 20, RATTATA
-	db 15, ABRA
-	db 19, ABRA
+	db 15, VULPIX
+	db 19, GROWLITHE
 	db 24, PIDGEOTTO
-	db 19, JIGGLYPUFF
-	db 24, JIGGLYPUFF
+	db 19, LICKITUNG
+	db 24, LICKITUNG
 	db 20, KADABRA
 	db 27, KADABRA
 	end_grass_wildmons
