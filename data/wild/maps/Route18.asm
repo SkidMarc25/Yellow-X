7fec9eab7a483209d94e1c666e06e2d8bf0ebd57
SkidMarc25 <marcstuts08@gmail.com>
2025-07-17 22:13:14 -0500

    Update Route18.asm

--- data/wild/maps/Route18.asm
+++ data/wild/maps/Route18.asm
@@ -2,9 +2,9 @@ Route18WildMons:
 	def_grass_wildmons 25 ; encounter rate
 	db 22, SPEAROW
 	db 22, DODUO
-	db 23, RATTATA
-	db 24, DODUO
-	db 24, RATTATA
+	db 23, VENONAT
+	db 24, KOFFING
+	db 24, GRIMER
 	db 26, DODUO
 	db 23, SPEAROW
 	db 24, FEAROW
