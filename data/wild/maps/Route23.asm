4757f982a237d9ddeeec4fad7495a12f7dff274f
SkidMarc25 <marcstuts08@gmail.com>
2025-07-17 22:22:21 -0500

    Update Route23.asm

--- data/wild/maps/Route23.asm
+++ data/wild/maps/Route23.asm
@@ -1,15 +1,15 @@
 Route23WildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 41, NIDORINO
-	db 41, NIDORINA
+	db 41, DODUO
+	db 41, PONYTA
 	db 36, MANKEY
 	db 44, NIDORINO
 	db 44, NIDORINA
 	db 40, FEAROW
-	db 41, MANKEY
-	db 45, FEAROW
+	db 41, MAROWAK
+	db 45, ARBOK
 	db 41, PRIMEAPE
-	db 46, PRIMEAPE
+	db 46, DODRIO
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
