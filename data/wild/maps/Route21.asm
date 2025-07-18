d655d175e050346501cba3c435aaa8d84fb2d58f
SkidMarc25 <marcstuts08@gmail.com>
2025-07-17 22:21:18 -0500

    Update Route21.asm

--- data/wild/maps/Route21.asm
+++ data/wild/maps/Route21.asm
@@ -2,25 +2,25 @@ Route21WildMons:
 	def_grass_wildmons 25 ; encounter rate
 	db 15, PIDGEY
 	db 13, RATTATA
-	db 13, PIDGEY
-	db 11, PIDGEY
-	db 17, PIDGEY
-	db 15, RATTATA
+	db 13, PIKACHU
+	db 11, SHELLDER
+	db 17, MR_MIME
+	db 15, TANGELA
 	db 15, RATICATE
-	db 17, PIDGEOTTO
+	db 17, PIKACHU
 	db 19, PIDGEOTTO
 	db 15, PIDGEOTTO
 	end_grass_wildmons
 
 	def_water_wildmons 5 ; encounter rate
 	db  5, TENTACOOL
-	db 10, TENTACOOL
+	db 10, KRABBY
 	db 15, TENTACOOL
 	db  5, TENTACOOL
-	db 10, TENTACOOL
+	db 10, HORSEA
 	db 15, TENTACOOL
-	db 20, TENTACOOL
+	db 20, SHELLDER
 	db 30, TENTACOOL
 	db 35, TENTACOOL
-	db 40, TENTACOOL
+	db 40, TENTACRUEL
 	end_water_wildmons
