3dec996dba65a5c2644f6cc586016244c13d3125
SkidMarc25 <marcstuts08@gmail.com>
2025-07-17 22:19:51 -0500

    Update Route20.asm

--- data/wild/maps/Route20.asm
+++ data/wild/maps/Route20.asm
@@ -3,14 +3,14 @@ Route20WildMons:
 	end_grass_wildmons
 
 	def_water_wildmons 5 ; encounter rate
-	db  5, TENTACOOL
+	db 15, HORSEA
 	db 10, TENTACOOL
 	db 15, TENTACOOL
-	db  5, TENTACOOL
-	db 10, TENTACOOL
-	db 15, TENTACOOL
+	db 15, STARYU
+	db 10, SEEL
+	db 15, SEEL
 	db 20, TENTACOOL
-	db 30, TENTACOOL
+	db 30, SHELLDER
 	db 35, TENTACOOL
 	db 40, TENTACOOL
 	end_water_wildmons
