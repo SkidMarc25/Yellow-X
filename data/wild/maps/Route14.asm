826755b3995cfd50c9e091f3255b0a629d0f068b
SkidMarc25 <marcstuts08@gmail.com>
2025-07-17 22:15:42 -0500

    Update Route14.asm

--- data/wild/maps/Route14.asm
+++ data/wild/maps/Route14.asm
@@ -4,12 +4,12 @@ Route14WildMons:
 	db 26, BELLSPROUT
 	db 24, VENONAT
 	db 30, PIDGEOTTO
-	db 28, ODDISH
-	db 28, BELLSPROUT
+	db 28, LICKITUNG
+	db 28, TANGELA
 	db 30, GLOOM
 	db 30, WEEPINBELL
-	db 27, VENONAT
-	db 30, VENOMOTH
+	db 27, EEVEE
+	db 30, EEVEE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
