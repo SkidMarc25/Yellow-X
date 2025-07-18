5fb6b0fb80b4483646742bdbaad62ff0ea0a05c4
SkidMarc25 <marcstuts08@gmail.com>
2025-07-18 06:02:03 -0500

    Update aerodactyl.asm

--- data/pokemon/base_stats/aerodactyl.asm
+++ data/pokemon/base_stats/aerodactyl.asm
@@ -17,7 +17,8 @@
 	tmhm RAZOR_WIND,   WHIRLWIND,    TOXIC,        TAKE_DOWN,    DOUBLE_EDGE,  \
 	     HYPER_BEAM,   RAGE,         DRAGON_RAGE,  MIMIC,        DOUBLE_TEAM,  \
 	     REFLECT,      BIDE,         FIRE_BLAST,   SWIFT,        SKY_ATTACK,   \
-	     REST,         SUBSTITUTE,   FLY
+	     REST,         SUBSTITUTE,   FLY,	ROCK_SLIDE,	EARTHQUAKE,	\
+	BODY_SLAM,	FISSURE,	SKULL_BASH
 	; end
 
 	db 0 ; padding
