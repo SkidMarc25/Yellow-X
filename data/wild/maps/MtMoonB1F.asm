9d9cd92aa214f834a782352aaf6f865c8f347013
SkidMarc25 <marcstuts08@gmail.com>
2025-07-17 22:00:55 -0500

    Update MtMoonB1F.asm

--- data/wild/maps/MtMoonB1F.asm
+++ data/wild/maps/MtMoonB1F.asm
@@ -1,12 +1,12 @@
 MtMoonB1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db  8, ZUBAT
+	db  8, PARAS
 	db  9, ZUBAT
 	db 10, ZUBAT
 	db 10, GEODUDE
 	db 11, GEODUDE
 	db 11, ZUBAT
-	db  9, PARAS
+	db  9, ONIX
 	db 11, PARAS
 	db 10, CLEFAIRY
 	db 12, CLEFAIRY
