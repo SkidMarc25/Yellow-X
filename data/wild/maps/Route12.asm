d4c1b351bf80a089f46de6351b97cbc2b5b82671
SkidMarc25 <marcstuts08@gmail.com>
2025-07-17 22:18:04 -0500

    Update Route12.asm

--- data/wild/maps/Route12.asm
+++ data/wild/maps/Route12.asm
@@ -1,11 +1,11 @@
 Route12WildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 25, ODDISH
-	db 25, BELLSPROUT
-	db 28, PIDGEY
+	db 25, POLIWAG
+	db 25, EKANS
+	db 28, PIKACHU
 	db 28, PIDGEOTTO
-	db 27, ODDISH
-	db 27, BELLSPROUT
+	db 27, PSYDUCK
+	db 27, KRABBY
 	db 29, GLOOM
 	db 29, WEEPINBELL
 	db 26, FARFETCHD
