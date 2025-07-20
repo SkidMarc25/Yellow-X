43f09eed4afe4a3c2abd5d52363050e6ed7fcd68
SkidMarc25 <marcstuts08@gmail.com>
2025-07-19 21:15:39 -0500

    Update slot_machine.asm
    Fixed Lucky Slot Machine scoring

--- engine/slots/slot_machine.asm
+++ engine/slots/slot_machine.asm
@@ -306,7 +306,8 @@ SlotMachine_StopWheel1Early:
 .loop
 	ld a, [hli]
 	cp HIGH(SLOTS7)
-	jr c, .stopWheel ; condition never true
+	;jr c, .stopWheel ; condition never true
+	jr z, .stopWheel
 	dec c
 	jr nz, .loop
 	ret
@@ -331,9 +332,14 @@ SlotMachine_StopWheel2Early:
 ; player's odds.
 .sevenAndBarMode
 	call SlotMachine_FindWheel1Wheel2Matches
+	ret nz
 	ld a, [de]
 	cp HIGH(SLOTSBAR) + 1
-	ret nc
+	;ret nc
+	jr c, .stopWheel
+	ld a, [wSlotMachineFlags]
+	bit 6, a
+	ret z
 .stopWheel
 	xor a
 	ld [wSlotMachineWheel2SlipCounter], a
