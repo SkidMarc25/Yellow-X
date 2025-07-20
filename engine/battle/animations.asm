3f8742a239619c49a55888a5122b6d369328d167
SkidMarc25 <marcstuts08@gmail.com>
2025-07-19 21:11:58 -0500

    Update animations.asm
    Boulder Strength Cloud Fix

--- engine/battle/animations.asm
+++ engine/battle/animations.asm
@@ -1413,9 +1413,10 @@ AdjustOAMBlockYPos2:
 	add b
 	cp 112
 	jr c, .skipSettingPreviousEntrysAttribute
-	dec hl
-	ld a, 160 ; bug, sets previous OAM entry's attribute
-	ld [hli], a
+	;dec hl
+	;ld a, 160 ; bug, sets previous OAM entry's attribute
+	ld a, 160
+	;ld [hli], a
 .skipSettingPreviousEntrysAttribute
 	ld [hl], a
 	add hl, de
